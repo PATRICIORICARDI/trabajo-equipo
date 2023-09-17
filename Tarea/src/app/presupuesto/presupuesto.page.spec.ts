93b7b551aae62e5ab156df4c72f928e1a4c3e405
PATRICIORICARDI <patricio.ricardi@gmail.com>
2023-09-17 12:25:42 -0600

    Practica2 - Presupuesto
    Presupuesto

---
+++ Tarea/src/app/presupuesto/presupuesto.page.spec.ts
@@ -0,0 +1,17 @@
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { PresupuestoPage } from './presupuesto.page';
+
+describe('PresupuestoPage', () => {
+  let component: PresupuestoPage;
+  let fixture: ComponentFixture<PresupuestoPage>;
+
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(PresupuestoPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
